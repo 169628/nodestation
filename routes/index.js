a872bd44a9e4896d383b99676bda0cda60d8bd68
Rainbow Lai <rainbow.pipi.1986@outlook.com>
2023-06-23 17:47:58 +0800

    other age

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/home", function (req, res, next) {
+  res.render("index");
+});
+
+router.get("/login", function (req, res, next) {
+  res.render("login");
+});
+
+router.get("/register", function (req, res, next) {
+  res.render("register");
+});
+
+router.get("/user", function (req, res, next) {
+  res.render("user");
+});
+
+router.get("/create", function (req, res, next) {
+  res.render("create");
+});
+
+router.get("/save", function (req, res, next) {
+  res.render("save");
+});
+
+router.get("/follow", function (req, res, next) {
+  res.render("follow");
+});
+
+router.get("/update", function (req, res, next) {
+  res.render("update");
 });
 
 module.exports = router;
